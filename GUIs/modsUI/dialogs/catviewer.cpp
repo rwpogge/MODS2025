027e3b64d14982bb5ad86ed75d559d5b760b6fb7
Richard Pogge <rwpogge@gmail.com>
2024-01-26 16:47:55 -0500

    Add files via upload

---
+++ GUIs/modsUI/dialogs/catviewer.cpp
@@ -0,0 +1,318 @@
+#include <QtGui>
+
+#include "mods.h"
+#include "catviewer.h"
+#include "actionbutton.h"
+#include "textentry.h"
+
+//
+// A modeless dialog to load and browse target and guide star catalogs
+// Uses a QTableWidget convenience class, and for now we adopt the Lucifer
+// catalog format (entries separated by |)
+//
+
+CatalogViewer::CatalogViewer(const QString &myTitle, QWidget *parent) 
+  : QDialog(parent)
+{
+  
+  // File button, Catalog Name label, and clear button across the top
+
+  fileButton = new ActionButton(tr("File..."),this);
+  connect(fileButton, SIGNAL(clicked()), this, SLOT(loadCatalog()));
+
+  catalogLabel = new QLabel("");
+  catalogLabel->setFont(QFont("Helvetica",appFontSize+2,QFont::Normal));
+
+  clearButton = new ActionButton(tr("Clear"),this);
+  connect(clearButton, SIGNAL(clicked()), this, SLOT(clearCatalog()));
+
+  int tbw = qMax((int)(fileButton->minimumSizeHint().width()),
+		 (int)(clearButton->minimumSizeHint().width()));
+  fileButton->setMinimumWidth(tbw);
+  clearButton->setMinimumWidth(tbw);
+
+  QHBoxLayout *topLayout = new QHBoxLayout;
+  topLayout->addWidget(fileButton);
+  topLayout->addStretch();
+  topLayout->addWidget(catalogLabel);
+  topLayout->addStretch();
+  topLayout->addWidget(clearButton);
+
+  // Create a blank QTableWidget for the catalog viewer, load the
+  // header, and connect its signals for item selection and click
+
+  catalogTable = new QTableWidget(this);
+  catalogTable->clear();
+  catalogTable->setColumnCount(MODS_CATALOG_NCOLS);
+  catalogTable->setSortingEnabled(false);
+  QStringList catHeader;
+  catHeader << "Target" << "RA" << "Dec" << "pmRA" << "pmDec" << "Equinox" << "Epoch" << "PA" << "Guide" << "RA" << "Dec" << "pmRA" << "pmDec" << "Equinox" << "Epoch" << "Rmag" << "B-R";
+  catalogTable->setHorizontalHeaderLabels(catHeader); 
+  catalogTable->horizontalHeader()->setResizeMode(QHeaderView::Stretch);
+  catalogTable->horizontalHeader()->resizeSections(QHeaderView::ResizeToContents);
+  catalogTable->resizeColumnsToContents();
+  connect(catalogTable,SIGNAL(itemSelectionChanged()),this,SLOT(selection()));
+  connect(catalogTable,SIGNAL(itemClicked(QTableWidgetItem *)),
+	  this,SLOT(select(QTableWidgetItem *)));
+
+  // Search tool: TextEntry to search for a target by name
+
+  findEntry = new TextEntry(tr("Find:"),"",12,this);
+  connect(findEntry,SIGNAL(dataReady(const QString &)),
+	  this,SLOT(findTarget(const QString &)));
+
+  findStatus = new QLabel("");
+  findStatus->clear();
+
+  // Commit and Dismiss buttons.  Commit does not become active until
+  // a selection has been made.
+
+  commitButton = new ActionButton(tr("Commit"),this);
+  connect(commitButton, SIGNAL(clicked()), this, SLOT(commit()));
+  commitButton->disable();
+
+  dismissButton = new ActionButton(tr("Dismiss"),this);
+  connect(dismissButton, SIGNAL(clicked()), this, SLOT(dismissCatalog()));
+
+  commitButton->setMinimumWidth((int)(dismissButton->minimumSizeHint().width()));
+
+  QHBoxLayout *buttonLayout = new QHBoxLayout;
+  buttonLayout->addWidget(findEntry);
+  buttonLayout->addWidget(findStatus);
+  buttonLayout->addStretch();
+  buttonLayout->addWidget(commitButton);
+  buttonLayout->addWidget(dismissButton);
+
+  // Vertical layout of the dialog
+
+  QVBoxLayout *viewerLayout = new QVBoxLayout;
+  viewerLayout->addLayout(topLayout);
+  viewerLayout->addWidget(catalogTable);
+  viewerLayout->addLayout(buttonLayout);
+  setLayout(viewerLayout);  
+
+  // Put up the decor (size it), and let it go
+
+  setWindowTitle(myTitle);
+  setMinimumSize(720,320);
+  // setMinimumSize(width,height);
+
+  // Allow the user to resize the window 
+
+  setSizeGripEnabled(true);
+  firstShow = true;
+  haveCatalog = false;
+  catalog.clear();
+}
+
+// hideEvent for this dialog, so we can save the catalog geometry when hidden
+
+void CatalogViewer::hideEvent(QHideEvent *)
+{
+  geom = geometry();
+}
+
+// showEvent for this dialog, so we can restore the catalog's geometry
+// when it is shown again
+
+void CatalogViewer::showEvent(QShowEvent *)
+{
+  if (!firstShow)
+    setGeometry(geom);
+  firstShow = false;
+}
+
+// Clear the catalog viewer
+
+void CatalogViewer::clearCatalog()
+{
+  catFileName.clear(); // forget which catalog we are using
+  commitButton->disable(); // disable the commit button
+  catalogLabel->clear();
+  catalogTable->setSortingEnabled(false);
+  for (int i=0;i<catalogTable->rowCount();i++)
+    catalogTable->removeRow(i);
+  haveCatalog = false;
+  catalogTable->clearContents(); // clear everything but the header
+  catalogTable->setRowCount(0);
+  findEntry->clear();
+  findEntry->setNormal();
+  findStatus->clear();
+}
+
+// Dismiss the catalog viewer
+
+void CatalogViewer::dismissCatalog()
+{
+  hide();
+}
+
+// Load a catalog into the viewer
+
+void CatalogViewer::loadCatalog()
+{
+  QString fileName;
+  QString catLine;
+
+  fileName = QFileDialog::getOpenFileName(this, 
+					  tr("Open a Target Catalog"),
+					  QString("%1/Catalogs").arg(MODS_BASE_PATH), 
+					  tr("Catalog Files (*.cat);;All Files (*.*)"));
+  if (!fileName.isEmpty()) {
+    QFile file(fileName);
+    if (file.open(QFile::ReadOnly | QFile::Text)) {
+      QTextStream stream(&file);
+      catalogTable->setSortingEnabled(false);
+      QString line;
+      if (catalogTable->rowCount() > 0) {
+	for (int i=0;i<catalogTable->rowCount();i++)
+	  catalogTable->removeRow(i);
+	clearCatalog();  // clear the current catalog
+      }
+      catalog.clear();
+      catalogTable->setRowCount(0);
+      int row = 0;
+      do {
+	line = stream.readLine();
+	catLine = line.simplified().replace(":"," ");  // simplify and remove any :'s
+	if (!catLine.isEmpty() && 
+	    !catLine.startsWith("#",Qt::CaseInsensitive)) {
+	  QStringList bits = catLine.split("|");
+	  QString catStr;
+	  catStr.clear();
+	  if (bits.size() == MODS_CATALOG_NCOLS) { // right number of bits
+	    catalogTable->insertRow(row);
+	    for (int col=0;col<MODS_CATALOG_NCOLS;col++) {
+	      QString itemStr = bits.at(col).simplified();
+	      if (col == 1 || col == 2 || col == 4 || col == 5)
+		itemStr.replace(" ",":");
+	      QTableWidgetItem *item = new QTableWidgetItem(itemStr);
+	      item->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEnabled);
+	      catalogTable->setItem(row,col,item);
+	      if (col > 0)
+		catStr.append(QString("|%1").arg(itemStr));
+	      else
+		catStr = itemStr;
+	    }
+
+	    // Create the hash table entry. The hash key is the
+	    // lowercase target name to permit case-insensitive
+	    // searching, and the hash table value is the laundered
+	    // catalog entry string
+
+	    catalog.insert(bits.at(0).simplified().toLower(),catStr);
+
+	  }
+	  row++;
+	}
+      } while (!line.isNull());
+
+      // Done reading, clean up
+
+      QFileInfo fileInfo(file);
+      catFileName = fileInfo.fileName();
+      catalogLabel->setText(catFileName);
+
+      // Resize the table and sort by target RA
+
+      //      catalogTable->horizontalHeader()->resizeSections(QHeaderView::Stretch);
+      catalogTable->horizontalHeader()->resizeSections(QHeaderView::ResizeToContents);
+      catalogTable->setSortingEnabled(true);
+      catalogTable->sortByColumn(1,Qt::AscendingOrder);
+      catalogTable->resizeColumnsToContents();
+      haveCatalog = true;
+
+      file.close();
+    }
+  }
+}
+
+// Get a catalog selection (response to single-click) but do not commit.
+// Instead, enable the commit button
+
+void CatalogViewer::select(QTableWidgetItem *item) 
+{
+  if (!haveCatalog) return;  // can't select if no catalog...
+  int row = item->row();
+  if (row < 0 || row > catalog.count()) return;
+  QString targKey = catalogTable->item(row,0)->text().toLower();
+  catEntry = catalog.value(targKey);
+  catalogTable->setCurrentCell(row,0,
+			       QItemSelectionModel::Rows | 
+			       QItemSelectionModel::ClearAndSelect);
+  commitButton->enable();
+  commitButton->setChange();
+  findEntry->clear();
+  findEntry->setNormal();
+  findStatus->clear();
+}
+
+// Commit a catalog selection
+
+void CatalogViewer::commit()
+{
+  if (!haveCatalog) return; // can't commit if we have no catalog
+  if (catEntry.isEmpty()) return; // don't send blank entries (sanity)
+  emit newTarget(catEntry);
+  commitButton->setNormal();
+}
+
+// Select a catalog entry
+
+void CatalogViewer::selection()
+{
+  int count = catalogTable->selectedRanges().count();
+  if (count == 0) return;
+  int rowcount = catalogTable->selectedRanges().at(0).rowCount();
+  if (rowcount == 1) {
+    int row = catalogTable->selectedRanges().at(0).topRow();
+    if (row < 0 || row > catalog.count()) return;
+    QString targKey = catalogTable->item(row,0)->text().toLower();
+    catEntry = catalog.value(targKey);
+    catalogTable->setCurrentCell(row,0,
+				 QItemSelectionModel::Rows | 
+				 QItemSelectionModel::ClearAndSelect);
+    commitButton->enable();
+    commitButton->setChange();
+    findEntry->clear();
+    findEntry->setNormal();
+    findStatus->clear();
+  }
+  else {
+    catEntry.clear();
+    commitButton->disable();
+  }
+}
+
+// Search the catalog for the given target name (case insensitive) and
+// select it
+
+void CatalogViewer::findTarget(const QString &findStr)
+{
+  if (!haveCatalog) return;
+  QString targName = findStr.simplified().toLower();
+  findEntry->setNormal();
+  findStatus->clear();
+  if (targName.isEmpty()) return;
+  if (catalog.count() == 0) return;
+
+  if (catalog.contains(targName)) {
+    catEntry = catalog.value(targName);
+    for (int i=0;i<catalog.size();i++) {
+      QString targStr = catalogTable->item(i,0)->text();
+      if (targStr.compare(targName,Qt::CaseInsensitive)==0) {
+	catalogTable->setCurrentCell(i,0,
+				     QItemSelectionModel::Rows | 
+				     QItemSelectionModel::ClearAndSelect);
+	catalogTable->scrollToItem(catalogTable->item(i,0),
+				   QAbstractItemView::PositionAtCenter);
+	commitButton->enable();
+	commitButton->setChange();
+	return;
+      }
+    }
+  }
+  findEntry->setEntryColor(QColor(255,0,0,127));
+  commitButton->disable();
+  findStatus->setText(tr("Target not found"));
+}
---
+++ GUIs/modsUI/dialogs/commlog.cpp
@@ -0,0 +1,160 @@
+#include <QtGui>
+
+#include "commlog.h"
+
+//
+// CommLog is a modeless dialog to show data-taking system message
+// traffic in a read-only QTextEdit window.  Messages are color coded
+// by IMPv2 message type, but other types of messages can be sent and
+// coded by a specified color.  This dialog box is used mostly for
+// showing data-taking system interprocess communcations logs, but we
+// can also use it to display other types of dynamic logs, for example
+// it is used by the envpanel for sensor alert logging and display.
+//
+
+CommLog::CommLog(const QString &logTitle, int width, int height,
+		 QWidget *parent) 
+  : QDialog(parent)
+{
+
+  // Big text widget, readonly, to show the message traffic
+
+  commDisplay = new QTextEdit(this);
+  commDisplay->setReadOnly(TRUE);
+  commDisplay->setLineWrapMode(QTextEdit::NoWrap);
+
+  // Clear and Dismiss buttons
+
+  clearButton = new QPushButton("Clear",this);
+  connect(clearButton, SIGNAL(clicked()), this, SLOT(clearLog()));
+  clearButton->setToolTip(tr("Clear the contents of this log window"));
+  dismissButton = new QPushButton("Dismiss",this);
+  connect(dismissButton, SIGNAL(clicked()), this, SLOT(dismissLog()));
+  dismissButton->setToolTip(tr("Dismiss (Hide) this log window"));
+  dismissButton->setDefault(true);  // Make sure this is the default button
+
+  QHBoxLayout *buttonLayout = new QHBoxLayout;
+  buttonLayout->addStretch();
+  buttonLayout->addWidget(clearButton);
+  buttonLayout->addWidget(dismissButton);
+
+  // Vertical layout with the log viewer and status layout over/under
+
+  QVBoxLayout *commLayout = new QVBoxLayout;
+  commLayout->addWidget(commDisplay);
+  commLayout->addLayout(buttonLayout);
+  setLayout(commLayout);  
+
+  // put up the decor, size it, and let it go
+
+  setWindowTitle(logTitle);
+  setMinimumSize(width,height);
+
+  // Allow the user to resize the window 
+
+  setSizeGripEnabled(true);
+
+  firstShow = true;
+  timeInMSec = true;
+
+}
+
+// Append a text message string to the log viewer with the specified
+// color.  This function takes care of inserting a date/time tag for
+// when it was logged.
+
+void CommLog::append(const QString &txt, const QColor &color)
+{
+
+  if (txt.isEmpty()) return;
+
+  // Work with a copy so we can clean it up a little...
+
+  QString commStr;
+  commStr = txt;
+  commStr.remove('\r');  // strip off any terminator characters
+
+  // Put a timetag on the msgText string we use for display purposes
+
+  QDateTime dateTime = QDateTime::currentDateTime().toUTC();
+
+  QString msgText;
+
+  if (timeInMSec)
+    msgText = QString("%1 %2").arg(dateTime.toString("[yyyy-MM-ddThh:mm:ss.zzz]")).arg(commStr);
+  else
+    msgText = QString("%1 %2").arg(dateTime.toString("[yyyy-MM-ddThh:mm:ss]")).arg(commStr);
+
+  // Color code the entry by message type
+
+  if (commStr.contains("ERROR:",Qt::CaseInsensitive))
+    commDisplay->setTextColor(Qt::red);
+  else if (commStr.contains("WARNING:",Qt::CaseInsensitive))
+    commDisplay->setTextColor(QColor(255,165,0,255));  // orange
+  else if (commStr.contains("FATAL:",Qt::CaseInsensitive) ||
+	   commStr.contains("CRITCAL:",Qt::CaseInsensitive))
+    commDisplay->setTextColor(Qt::magenta);
+  else if (commStr.contains("STATUS:",Qt::CaseInsensitive))
+    commDisplay->setTextColor(Qt::blue);
+  else if (commStr.contains("EXEC:",Qt::CaseInsensitive))
+    commDisplay->setTextColor(Qt::darkYellow);
+  else if (commStr.contains("DONE:",Qt::CaseInsensitive))
+    commDisplay->setTextColor(Qt::darkGreen);
+  else
+    commDisplay->setTextColor(color);
+
+  // And put it into the log display, setting the cursor so that the
+  // latest entry is visible in the slider box
+
+  commDisplay->append(msgText);
+  commDisplay->setTextColor(Qt::black); 
+  commDisplay->moveCursor(QTextCursor::End);
+  commDisplay->moveCursor(QTextCursor::StartOfLine);
+}
+
+// hideEvent for this dialog so we can save the log geometry when it
+// is hidden
+
+void CommLog::hideEvent(QHideEvent *)
+{
+  geom = geometry();
+}
+
+// showEvent for this dialog, so we can restore the comm log's
+// geometry when it is shown again
+
+void CommLog::showEvent(QShowEvent *)
+{
+  if (!firstShow)
+    setGeometry(geom);
+  firstShow = false;
+}
+
+// Clear the log window (can be used as a slot)
+
+void CommLog::clearLog()
+{
+  commDisplay->clear();
+}
+
+// Dismiss (aka Hide) the log window - emits the logHidden() signal
+
+void CommLog::dismissLog()
+{
+  hide();
+  emit logHidden();
+}
+
+// Change timetag precision to milliseconds
+
+void CommLog::millisec()
+{
+  timeInMSec = true;
+}
+
+// Change timetag precision to seconds
+
+void CommLog::seconds()
+{
+  timeInMSec = false;
+}
