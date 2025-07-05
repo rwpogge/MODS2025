7b63d6de8fd424d72d9b2dd2d8c4f18bc38f577f
Richard Pogge <rwpogge@gmail.com>
2025-07-05 15:54:54 -0400

    Add files via upload

---
+++ agw/agwUtils/mods_init.c
@@ -0,0 +1,87 @@
+/** \file mods_init.c
+ * \brief Client for initializing MODS AGW
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory.h>
+
+#include "../include/islapi.h"
+#include "../include/modscontrol.h"
+
+extern sac_xy xy;
+
+#define BUFFSIZE	512
+extern char *mods_fatalerrormsg[];
+
+int mods_init(char *host, int agw_no, int options)
+{
+  islcomp comp;
+  islnum app;
+  islconn conn;
+  char errchar[4];
+  char send_buff[BUFFSIZE];
+  char recv_buff[BUFFSIZE];
+  int bcnt;
+  int len;
+  int i;
+  int ierr;
+
+  ierr=0;
+  memset(send_buff,0,sizeof(send_buff));
+  memset(recv_buff,0,sizeof(recv_buff));
+  comp = islCnameToComp(host); // convert arguments to binary format comp
+  if (comp == -1) {
+    ierr=1;
+    sprintf(recv_buff,"Network Error, *NO* such host named <%s>",host);
+    mods_fatalerrormsg[ierr]=(char *)recv_buff;
+    return(ierr);
+  }
+
+  if (MODS_1_PORT != 0)
+    if(!agw_no)
+      app = (islnum) MODS_1_PORT; // convert argument to binary format islnum
+    else
+      app = (islnum) MODS_2_PORT; // convert argument to binary format islnum
+  else
+    if ((app = islAppNameToAppNumb("echo")) == -1)
+      return(2);
+  
+  conn = islMakeContact(comp, app); // form connection to the agwService
+  if (conn < 0) {
+    ierr=3;
+    sprintf(recv_buff,"Comm. Error, *NO* Connection formed on <%s>",host);
+    mods_fatalerrormsg[ierr]=(char *)recv_buff;
+    return ierr;
+  }  
+  (void) fflush(stdout);
+
+  sprintf(send_buff,"init %d",options);
+  len=strlen(send_buff)+1;
+  (void) send(conn, send_buff, len+1, 0); // send to agwServer
+  (void) fflush(stdout);
+  
+  len = recv(conn, recv_buff, sizeof(recv_buff), 0); // read from agwServer
+  recv_buff[len]='\0'; // prevent segmentation faults.
+
+  if (len < 0) {
+    ierr=4;
+    sprintf(recv_buff,"Comm. Error: read from <%s> *FAILED*",host);
+    mods_fatalerrormsg[ierr]=(char *)recv_buff;
+
+  } else if(strstr(recv_buff,"ERROR:")) { // check for error
+    for(bcnt=0;recv_buff[bcnt]!=':';bcnt++); // Search for ':' in string.
+    bcnt++;
+
+    strncpy(errchar,&recv_buff[bcnt],3); // get error number.
+    ierr=atoi(errchar);
+
+    mods_fatalerrormsg[ierr]=(char *)&recv_buff[bcnt+3];
+  }
+
+  ierr=initTrans(host,agw_no,&xy);
+
+  (void) fflush(stdout);
+  (void) islSendEOF(conn);  // iteration ends when EOF found
+  return ierr;
+}
---
+++ agw/agwUtils/mods_setfocus.c
@@ -0,0 +1,85 @@
+/** \file mods_setfocus.c
+ * \brief Client stub for driving the Focus to a new position
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory.h>
+
+#include "../include/islapi.h"
+#include "../include/modscontrol.h"
+
+#define BUFFSIZE		512
+extern char *mods_fatalerrormsg[];
+
+int 
+mods_setfocus(char *host, int agw_no, double new_pos, double velocity, int options)
+{
+  islcomp comp;
+  islnum app;
+  islconn conn;
+  char errchar[4];
+  char send_buff[BUFFSIZE];
+  char recv_buff[BUFFSIZE];
+  int ierr;
+  int len;
+  int bcnt;
+
+  memset(send_buff,0,sizeof(send_buff));
+  memset(recv_buff,0,sizeof(recv_buff));
+  ierr=0;
+  comp = islCnameToComp(host); // convert arguments to binary format comp
+  if (comp == -1) {
+    ierr=1;
+    sprintf(recv_buff,"Network Error, *NO* such host named <%s>",host);
+    mods_fatalerrormsg[ierr]=(char *)recv_buff;
+    return(ierr);
+  }
+
+  if (MODS_1_PORT != 0)
+    if(!agw_no)
+      app = (islnum) MODS_1_PORT; // convert argument to binary format islnum
+    else
+      app = (islnum) MODS_2_PORT; // convert argument to binary format islnum
+  else
+    if ((app = islAppNameToAppNumb("echo")) == -1)
+      return(2);
+  
+  conn = islMakeContact(comp, app); // form connection to the agwService
+  if (conn < 0) {
+    ierr=3;
+    sprintf(recv_buff,"Comm. Error, *NO* Connection formed on <%s>",host);
+    mods_fatalerrormsg[ierr]=(char *)recv_buff;
+    return ierr;
+  }  
+  (void) fflush(stdout);
+
+  sprintf(send_buff,"setfocus %f %d",new_pos,options);
+  len=strlen(send_buff)+1;
+  send_buff[len]='\0';
+
+  (void) send(conn, send_buff, len, 0); // send msg to agwService
+  (void) fflush(stdout);
+    
+  len = recv(conn, recv_buff, sizeof(recv_buff), 0); // read from agwService
+  recv_buff[len]='\0'; // prevent segmentation faults.
+
+  if (len < 0) {
+    ierr=4;
+    sprintf(recv_buff,"Comm. Error: read from <%s> *FAILED*",host);
+    mods_fatalerrormsg[ierr]=(char *)recv_buff;
+
+  } else if(strstr(recv_buff,"ERROR:")) { // check for error
+    for(bcnt=0;recv_buff[bcnt]!=':';bcnt++); // Search for ':' in string.
+    bcnt++;
+
+    strncpy(errchar,&recv_buff[bcnt],3); // get error number.
+    ierr=atoi(errchar);
+
+    mods_fatalerrormsg[ierr]=(char *)&recv_buff[bcnt+3];
+  }
+  
+  (void) fflush(stdout);
+  (void) islSendEOF(conn); // iteration ends when EOF found
+  return ierr;
+}
