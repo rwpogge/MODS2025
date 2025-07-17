a0c0b688b861e59c834bc065ca69ec1957d027e3
Richard Pogge <rwpogge@gmail.com>
2025-07-17 15:19:04 -0400

    Update modsalloc.sh
    SELinux enforcing mode changes

--- mods/modsalloc/modsalloc.sh
+++ mods/modsalloc/modsalloc.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #
-file=/home/dts/mods/bin/modsalloc
+# SELinux enforcing mode means the executable needs to be in /usr/local/bin 
+# copied as sudo
+#
+file=/usr/local/bin/modsalloc
 if [ -f ${file} ]
 then
         ${file}
