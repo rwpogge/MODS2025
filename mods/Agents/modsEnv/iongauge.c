086e6cceb60667f43f407965b1831574d08ac75b
Richard Pogge <rwpogge@gmail.com>
2025-08-23 20:52:58 -0400

    Update iongauge.c
    cleaned up, removed test diagnostic connection at end of initIonSocket()

--- mods/Agents/modsEnv/iongauge.c
+++ mods/Agents/modsEnv/iongauge.c
@@ -24,15 +24,15 @@
 #define TIMEOUT_LENGTH 5                // How long to wait on a response from the gauge before timeout.
 
 /*!
-    \brief Send a command and recieve a response from the ion gauge.
+  \brief Send a command and recieve a response from the ion gauge.
     
-    \param socket TCP socket connected to the ion gauge
-    \param response string where the response data will be written.
-    \param command string with the command to send.
+  \param socket TCP socket connected to the ion gauge
+  \param response string where the response data will be written.
+  \param command string with the command to send.
     
-    \return 0 on success, -1 on errors.  
+  \return 0 on success, -1 on errors.  
     
-    Sends the given command to the ion gauge, and stores the response.
+  Sends the given command to the ion gauge, and stores the response.
 */
 
 int
@@ -52,118 +52,110 @@ sendIonCommand(int socket, char* command, char* response)
 }
 
 /*!
-    \brief Create a TCP socket which is connected to an ion-gauge at the given address:port.
+  \brief Create a TCP socket which is connected to an ion-gauge at the given address:port.
     
-    \param sock pointer to where the connected socket will be stored.
-    \param address string with the IP address to connect to.
-    \param port integer with the port to connect to.
-    \param timeout integer with the maximum number of seconds that should be spent trying to connect.
+  \param sock pointer to where the connected socket will be stored.
+  \param address string with the IP address to connect to.
+  \param port integer with the port to connect to.
+  \param timeout integer with the maximum number of seconds that should be spent trying to connect.
     
-    \return 0 on success, -1 on errors.  
+  \return 0 on success, -1 on errors.  
 */
 
 int
-initIonSocket(int* sock, char* address, int port, int timeout){
+initIonSocket(int* sock, char* address, int port, int timeout)
+{
   
-    // Create the TCP socket
+  // Create the TCP socket
   
-    (*sock) = socket(AF_INET, SOCK_STREAM, 0);
+  (*sock) = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Set the TCP socket address and port
+  // Set the TCP socket address and port
     
-    struct sockaddr_in ionAddr;
-    ionAddr.sin_family = AF_INET;
-    ionAddr.sin_addr.s_addr = inet_addr(address);
-    ionAddr.sin_port = htons(port);
+  struct sockaddr_in ionAddr;
+  ionAddr.sin_family = AF_INET;
+  ionAddr.sin_addr.s_addr = inet_addr(address);
+  ionAddr.sin_port = htons(port);
 
-    // Set the timeout
+  // Set the timeout
 
-    struct timeval timeoutStruct; 
-    timeoutStruct.tv_sec = timeout;
-    timeoutStruct.tv_usec = 0;
+  struct timeval timeoutStruct; 
+  timeoutStruct.tv_sec = timeout;
+  timeoutStruct.tv_usec = 0;
 
-    // Variables used for making the socket connection.
+  // Variables used for making the socket connection.
     
-    int result, opt;
+  int result, opt;
     
-    // Get socket flags
+  // Get socket flags
 
-    if ((opt = fcntl ((*sock), F_GETFL, NULL)) < 0)
-      return -1;
+  if ((opt = fcntl ((*sock), F_GETFL, NULL)) < 0)
+    return -1;
 
-    // Set socket non-blocking flag
+  // Set socket non-blocking flag
 
-    if (fcntl ((*sock), F_SETFL, opt | O_NONBLOCK) < 0)
-      return -1;
+  if (fcntl ((*sock), F_SETFL, opt | O_NONBLOCK) < 0)
+    return -1;
 
-    // Try to connect with a timeout
+  // Try to connect with a timeout
     
-    if ((result = connect(*sock, (struct sockaddr*) &ionAddr, sizeof(ionAddr)) < 0)){
-      if (errno == EINPROGRESS) {
-	fd_set wait_set;
-	// Make file descriptor set with socket
-	FD_ZERO (&wait_set);
-	FD_SET ((*sock), &wait_set);
-	// Wait for socket to be writable; return after given timeout.
-	result = select ((*sock) + 1, NULL, &wait_set, NULL, &timeoutStruct);
-      }
-
-    // Connection was successful immediately.
-
+  if ((result = connect(*sock, (struct sockaddr*) &ionAddr, sizeof(ionAddr)) < 0)){
+    if (errno == EINPROGRESS) {
+      fd_set wait_set;
+      // Make file descriptor set with socket
+      FD_ZERO (&wait_set);
+      FD_SET ((*sock), &wait_set);
+      // Wait for socket to be writable; return after given timeout.
+      result = select ((*sock) + 1, NULL, &wait_set, NULL, &timeoutStruct);
     }
-    else {
-      result = 1;
+  }
+  else { // Connection was successful immediately.
+    result = 1;
+  }
+
+  // Reset the socket non-blocking flag
+    
+  if (fcntl ((*sock), F_SETFL, opt) < 0) 
+    return -1;
+    
+  if (result < 0){ // An error occured in connect or select
+    return -1;
+  }
+  else if (result == 0) { // select timed out
+    errno = ETIMEDOUT;
+    return -1;
+  }
+  else { // no error 
+    socklen_t len = sizeof(opt);
+
+    setsockopt((*sock), SOL_SOCKET, SO_RCVTIMEO, (const char*) &timeoutStruct, sizeof(timeoutStruct));
+    setsockopt((*sock), SOL_SOCKET, SO_SNDTIMEO, (const char*) &timeoutStruct, sizeof(timeoutStruct));
+
+    // Check for errors in socket layer.
       
-    }
-
-    // Reset the socket non-blocking flag
-    
-    if (fcntl ((*sock), F_SETFL, opt) < 0) 
-      return -1;
-    
-    // An error occured in connect or select
-    
-    if (result < 0){
+    if (getsockopt ((*sock), SOL_SOCKET, SO_ERROR, &opt, &len) < 0)
       return -1;
 
-    // Select timed out
-      
-    }
-    else if (result == 0) {
-      errno = ETIMEDOUT;
-      return -1;
-    
-    // No error occured in connect or select
-      
-    }
-    else {
-      socklen_t len = sizeof(opt);
+    // There was an error.
 
-      setsockopt((*sock), SOL_SOCKET, SO_RCVTIMEO, (const char*) &timeoutStruct, sizeof(timeoutStruct));
-      setsockopt((*sock), SOL_SOCKET, SO_SNDTIMEO, (const char*) &timeoutStruct, sizeof(timeoutStruct));
-
-      // Check for errors in socket layer.
-      
-      if (getsockopt ((*sock), SOL_SOCKET, SO_ERROR, &opt, &len) < 0)
-	return -1;
-
-      // There was an error.
-      if(opt){
-	errno = opt;
-	return -1;
-      }
-    }
-
-    // Attempt an initial query to see if the gauge is on and
-    // physically connected to the Comtrol serial port server
-    
-    char response[MESSAGE_SIZE];
-
-    if (sendIonCommand((*sock), "#05VER\r", response) < 0) {
-      close(*sock);
+    if (opt) {
+      errno = opt;
       return -1;
     }
+  }
 
-    return 0;
+  /*
+  // For testing: attempt an initial query to see if the gauge is on
+  // and physically connected to the Comtrol serial port server
+  
+  char response[MESSAGE_SIZE];
+  
+  if (sendIonCommand((*sock), "#05VER\r", response) < 0) {
+  close(*sock);
+  return -1;
+  }
+  */
+
+  return 0;
 }
 
