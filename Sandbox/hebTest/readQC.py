027c610a22b8483d99b73349cc1736db95498137
Richard Pogge <rwpogge@gmail.com>
2025-08-26 20:49:35 -0400

    Create readQC.py
    new test program

---
+++ Sandbox/hebTest/readQC.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+'''
+Read out QCs, do basic stats.  Lightweight
+way to watch QCs during testing and tweaking
+when the stripchart plot is not going to work
+(e.g., long distance support)
+
+2025 Aug 26 - rwp/osu (last mod)
+'''
+
+import sys
+from time import sleep
+from datetime import datetime
+import time
+import numpy as np
+
+import warnings
+warnings.filterwarnings('ignore',category=UserWarning,append=True)
+
+#----------------------------------------------------------------
+#
+# utility functions
+#
+
+def getQCs(client,addr):
+    '''
+    get raw data from the quad cell
+    '''
+    numAI = 4
+    qcData = []
+    try:
+        rd = client.read_holding_registers(addr,count=numAI)
+    except Exception as ex:
+        for i in range(numAI):
+            qcData.append(-999.99)
+        return qcData
+    
+    qcData = rd.registers
+    return qcData
+
+def qc2vdc(rawQC):
+    '''
+    Convert raw quadcell data to a voltage (0..10VDC).
+    At 15-bits for the positive side, the digitization scale is ~0.3mV
+    '''
+    posMax = 2**15 - 1
+    negMin = 2**16 - 2
+    if rawQC > posMax:
+        Vout = 10.0*((rawQC-negMin)/posMax)
+    else:
+        Vout = 10.0*(rawQC/posMax)
+    return Vout
+
+def getRTDs(client,addr,num):
+    temp = []
+    try:
+        rd = client.read_input_registers(addr,num)
+    except Exception as ex:
+        for i in range(num):
+            temp.append(-99.99)
+        return False,temp
+
+    for i in range(num):
+        temp.append(ptRTD2C(float(rd.registers[i])))
+
+    return True,temp
+
+def ptRTD2C(rawRTD):
+    tempRes = 0.1
+    tempMax = 850.0
+    wrapT = tempRes*((2.0**16)-1)
+
+    temp = tempRes*rawRTD
+    if temp > tempMax:
+        temp -= wrapT
+
+    return temp
+
+#----------------------------------------------------------------
+'''
+Main program, of sorts.  
+
+We're being very slovenly here and defining the modbus TCP client
+instance before we define the getRTDs() function so we don't have to
+pass it the client handle.  Just asking for trouble...
+'''
+
+# Set up the Modbus I/O client as a synchronous TCP client
+
+from pymodbus.client import ModbusTcpClient as mbc
+
+# This is our WAGO unit on the ISL private subnet when the WAGO is
+# being tested standalone outside a DESI IEB in the lab at OSU - check
+# IP, may vary from unit to unit, but this is typical.
+
+m1rHost = '192.168.139.141' # MODS1 Red
+m1bHost = '192.168.139.142' # MODS1 Blue
+m2rHost = '192.168.139.241' # MODS2 Red
+m2bHost = '192.168.139.242' # MODS2 Blue
+m1dHost = '192.168.139.41'  # MODS1-dev Tucson Lab
+
+wagoHost = m2rHost
+
+# WAGO register addresses of the QC ADC module and the RTD sensor module
+
+qcAddr = 0
+rtdAddr = 4
+
+# Instantiate the client - interestingly the initializer does not
+# obviously raise exceptions if there are issues, e.g., if we give it
+# a bogus IP address.  That comes later when we try to connect
+
+try:
+    client = mbc(wagoHost)
+except:
+    print(f"*** ERROR: Could not initiate WAGO Modbus client on {wagoHost}")
+    sys.exit(1)
+
+# connect or fail
+
+if not client.connect():
+    print(f"*** ERROR: Could not connect to WAGO {wagoHost}")
+    sys.exit(1)
+
+# log file
+
+nt = datetime.now()
+runDate = nt.strftime("%Y%m%d")
+
+qcFile = f"qcData_{runDate}.csv"
+
+QCLog = open(qcFile,'w')
+QCLog.write("time,Tair,qc1,qc2,qc3,qc4\n")
+
+#--------------------------------
+#
+# Back to the main program
+#
+
+cadence = 1.0 # measurement cadence in seconds
+numMeas = 50 # number of measures (-1 for infinite loop)
+
+qc1d = []
+qc2d = []
+qc3d = []
+qc4d = []
+
+# One measurement each to get things started
+
+qcData = getQCs(client,qcAddr)
+qc1 = qc2vdc(qcData[0])
+qc1d.append(qc1)
+qc2 = qc2vdc(qcData[1])
+qc2d.append(qc2)
+qc3 = qc2vdc(qcData[2])
+qc3d.append(qc3)
+qc4 = qc2vdc(qcData[3])
+qc4d.append(qc4)
+status,temp = getRTDs(client,rtdAddr,1)
+t0 = time.time() # datetime.now()
+
+tMeas = 0.0
+#logStr = f"{tMeas:.6f},{temp[0]:.1f},{qc1:.5f},{qc2:.5f},{qc3:.5f},{qc4:.5f}"
+logStr = f"{qc1:.5f}  {qc2:.5f}  {qc3:.5f}  {qc4:.5f}"
+#logStr = f"{tMeas:.6f},{qcData[0]},{qcData[1]},{qcData[2]},{qcData[3]}"
+print(f"{logStr}")
+#QCLog.write(f"{logStr}\n")
+
+measCount = 0
+while(True):
+   sleep(cadence)
+   qcData = getQCs(client,qcAddr)
+   qc1 = qc2vdc(qcData[0])
+   qc2 = qc2vdc(qcData[1])
+   qc3 = qc2vdc(qcData[2])
+   qc4 = qc2vdc(qcData[3])
+   qc1d.append(qc1)
+   qc2d.append(qc2)
+   qc3d.append(qc3)
+   qc4d.append(qc4)
+   logStr = f"{qc1:.5f}  {qc2:.5f}  {qc3:.5f}  {qc4:.5f}"
+   print(f"{logStr}")
+   measCount += 1
+   if numMeas > 0 and measCount >= numMeas:
+       break
+
+if numMeas > 0:
+   medQ1 = np.median(qc1d)
+   sigQ1 = np.std(qc1d)
+   medQ2 = np.median(qc2d)
+   sigQ2 = np.std(qc2d)
+   medQ3 = np.median(qc3d)
+   sigQ3 = np.std(qc3d)
+   medQ4 = np.median(qc4d)
+   sigQ4 = np.std(qc4d)
+   print(f"QC1: {medQ1:.4f} +/- {sigQ1:.4f}")
+   print(f"QC2: {medQ2:.4f} +/- {sigQ2:.4f}")
+   print(f"QC3: {medQ3:.4f} +/- {sigQ3:.4f}")
+   print(f"QC4: {medQ4:.4f} +/- {sigQ4:.4f}")
+
+
+sys.exit(0)
