b04254e92a6f9d7594581298907409ea36dfb0f7
Richard Pogge <rwpogge@gmail.com>
2025-09-19 16:30:49 -0400

    Update main.c
    more oops

--- Sandbox/modsDD/main.c
+++ Sandbox/modsDD/main.c
@@ -152,7 +152,7 @@ main(int argc, char* argv[])
 
   // which side of LBT are we on?
   
-  if (!strncasecomp(lbt.side,"left"))
+  if (!strncasecmp(lbt.side,"left"))
     side = "L";
   else
     side = "R";
