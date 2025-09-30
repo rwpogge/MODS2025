769bf1b3063ad0fdc89c42dd95695295065115de
Richard Pogge <rwpogge@gmail.com>
2025-09-30 12:06:27 -0400

    modbusutils update
    Minor tweaks to modbusutils based on live testing

--- mods/Agents/modsEnv/modbusutils.c
+++ mods/Agents/modsEnv/modbusutils.c
@@ -7,6 +7,7 @@
 
   Modification History:
     2025 Aug 28 - added client.h and useCLI for print statements
+    2025 Sep 30 - tweaks from live testing [rwp/osu]
 */
 
 #include "modbusutils.h"
@@ -31,11 +32,16 @@ int
 wagoSetGet(int setGet, int isRegister, char *wagoAddr, int addr, int len, void* data)
 {
   // using libmodbus
+
   modbus_t *modbus;
   int result;
 
   // Open a Modbus/TCP connection to the WAGO
+  
   modbus = modbus_new_tcp(wagoAddr,502);
+
+  usleep(10000); // see if a beat before connect helps with timing related connection faults
+  
   if (modbus_connect(modbus) == -1) {
     if (useCLI) printf("ERROR: Cannot connect to WAGO host %s: %s\n",wagoAddr,modbus_strerror(errno));
     modbus_free(modbus);
@@ -43,34 +49,67 @@ wagoSetGet(int setGet, int isRegister, char *wagoAddr, int addr, int len, void* 
   }
 
   // Slight pause to give a slow TCP link a chance to catch up
+
   usleep(10000);
 
   // If setGet = 1 (true), we are writing data to WAGO registers/coils
+
   if (setGet){
-    if(isRegister) result = modbus_write_registers(modbus,addr,len,(uint16_t*)data);
-    else result = modbus_write_bits(modbus,addr,len,(uint8_t*)data);
+    if (isRegister)
+      result = modbus_write_registers(modbus,addr,len,(uint16_t*)data);
+    else
+      result = modbus_write_bits(modbus,addr,len,(uint8_t*)data);
   } 
 
   // If setGet = 0 (false), we are reading data from WAGO registers/coils
+  
   else{
-    if(isRegister) result = modbus_read_registers(modbus,addr,len,(uint16_t*)data);
-    else result = modbus_read_bits(modbus,addr,len,(uint8_t*)data);
+    if (isRegister)
+      result = modbus_read_registers(modbus,addr,len,(uint16_t*)data);
+    else
+      result = modbus_read_bits(modbus,addr,len,(uint8_t*)data);
   } 
     
   // close connection and free libmodbus context
+
   modbus_close(modbus);
   modbus_free(modbus);
 
   // Return status: 0 on success, -1 on errors with errno set by modbus_xxx_registers()
+
   if (result == -1) return -1;
   return 0;
 }
+
+/*!
+  \brief Write (set) or read (get) data from WAGO modbus coils
+
+  \param setget   (int) 1 = set (write), 0 = get (read)
+  \param wagoAddr (char*) The address of the WAGO device that should be queried.
+  \param coilAddr (int) coil address (range: 1 - 0x10000)
+  \param coilLen  (int) coil length = number of data values (range: 1-16)
+  \param coilData (uint8) array of data to write or read (0 values will write false, 1 values will write true).
+
+  \return 0 on send success, value or error code on faults
+*/
 
 int
 wagoSetGetCoils(int setGet, char *wagoAddr, int coilAddr, int coilLen, uint8_t coilData[])
 {
   return wagoSetGet(setGet, 0, wagoAddr, coilAddr, coilLen, coilData);
 }
+
+/*!
+  \brief Write (set) or read (get) data from WAGO modbus registers
+  
+  \param setget   (int) 1 = set (write), 0 = get (read)
+  \param wagoAddr (char*) The address of the WAGO device that should be queried.
+  \param regAddr  (int) register address (range: 1 - 0x10000)
+  \param regLen   (int) register length = number of data values (range: 1-100)
+  \param regData  (uint16) array of data to write or read
+  
+  \return 0 on send success, value or error code on faults
+*/
 
 int
 wagoSetGetRegisters(int setGet, char *wagoAddr, int regAddr, int regLen, uint16_t regData[])
