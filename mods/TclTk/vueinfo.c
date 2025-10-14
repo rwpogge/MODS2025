e744c95907807088e634b9c0c07f4a02b65b21b2
Richard Pogge <rwpogge@gmail.com>
2025-10-14 13:29:45 -0400

    Update vueinfo.c
    oops...

--- mods/TclTk/vueinfo.c
+++ mods/TclTk/vueinfo.c
@@ -473,7 +473,6 @@ main(int argc, char *argv[])
   }
   // environmental sensors
   else if (!strcasecmp(what,"env")) {
-    printf("%s Environmental Sensors:\n",ms->MODS.instID);
     printf("  Glycol Supply: P=%.2f psi-g  T=%.1f C\n",ms->MODS.glycolSupplyPressure,ms->MODS.glycolSupplyTemperature);
     printf("         Return: P=%.2f psi-g  T=%.1f C\n",ms->MODS.glycolReturnPressure,ms->MODS.glycolReturnTemperature);
     printf("  IUB Inside Air T=%.1f C  Ambient T=%.1f C  HeatSink T=%.1f C\n",
