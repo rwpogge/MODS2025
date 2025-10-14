da578f592ba4ab94ff43fdd7f0f76b5ded1f5af5
Richard Pogge <rwpogge@gmail.com>
2025-10-14 17:02:49 -0400

    tmux modsTerm
    cleanup

--- Sandbox/TMux/modsTerm.bash
+++ Sandbox/TMux/modsTerm.bash
@@ -1,36 +1,74 @@
 #!/bin/bash
+#
+# modsTerm - start MODS binocular observing script session
+#
+# This script launches a named tmux session that will be used to execute
+# MODS binocular science target acquisition and observation scripts.
+#
+# While it may be invoked by itself, if the session is not running
+# it will be executed by the acqBinoMODS and execBinoMODS scripts.
+#
+# The modsTerm tmux session is configured with 3 window panes
+# 
+#      +---------------------+---------------------+
+#      |        MODS1        |        MODS2        |
+#      |         1.0         |         1.1         |
+#      |                     |                     |
+#      |                     |                     |
+#      |                     |                     |
+#      +---------------------+---------------------+
+#      |                  command                  |
+#      |                    1.2                    |
+#      +---------------------+---------------------+
+#
+# Low-level setup is in the $configDir/tmux_mods.conf file.
+# Also defines and exports aliases to the bash shell to make
+# detachment by "detach" or "quit" and throttling "exit"
+#
+# R. Pogge (pogge.1@osu.edu)
+# OSU Astronomy Department
+# 2025 Oct 4 - Alpha version
+#
+# Modification History:
+#   2025 Oct 13 - beta version, put tmaliases in configDir [rwp/osu]
+#
+#---------------------------------------------------------------------------
 
 tmuxID="modsTerm"
-modsHost="${HOSTNAME^^}"
 
-# Check if the session already exists
+# LBTO Public executable path
 
-tmux has-session -t $tmuxID 2>/dev/null
+modsDir="/lbt/lbto/mods"
+configDir="/lbt/lbto/mods/.config"
 
+# Parse the command-line options
+
+tmux has-session -t $tmuxID 2> /dev/null
 if [ $? != 0 ]; then
-  # Create a new detached session
-
-  tmux -f ~/.tmux.conf.mods new-session -d -s $tmuxID
-
-  # Create a new window (if needed) and name it
-
-  tmux new-window -t $tmuxID:1 -n "${modsHost} Server Admin"
-
-  # Split the window into panes (optional, adjust as needed)
-
-  tmux split-window -v -p 5  # split bottom pane shorter
-  tmux split-window -h -p 50 -t $tmuxID:1.0 # Split horizontally
-
-  # Send commands to the panes to start programs
-
-  # tmux send-keys -t $tmuxID:1.0 "./modsStatus" C-m # run the ISIS server in the first pane
-
-  # Select the pane you want active initially
-
-  tmux select-pane -t $tmuxID:1.2 # attach the command pane
-
+   tmux -f ${configDir}/tmux_mods.conf new-session -d -s $tmuxID
+   tmux new-window -t ${tmuxID}:1 -n "MODS Binocular Observing"
+   tmux split-window -v -p 5  
+   tmux split-window -h -p 50 -t ${tmuxID}:1.0
+   # pane IDs
+   tmux set -g pane-border-status top
+   tmux set -g pane-border-format " #{pane_title} "
+   tmux select-pane -t ${tmuxID}:1.0 -T "MODS1"
+   tmux select-pane -t ${tmuxID}:1.1 -T "MODS2"
+   tmux select-pane -t ${tmuxID}:1.2 -T "Commands"
+   # aliases
+   tmux send-keys -t ${tmuxID}:1.0 "source ${configDir}/tmaliases" C-m
+   tmux send-keys -t ${tmuxID}:1.1 "source ${configDir}/tmaliases" C-m
+   tmux send-keys -t ${tmuxID}:1.2 "source ${configDir}/tmaliases" C-m
 fi
 
-# Attach to the session
+# If we're already in the expected tmux session, do nothing
+# otherwise attach to the command pane (1.2)
 
-tmux attach-session -t $tmuxID
+if [ -n "$TMUX" ]; then
+   echo "Currently in the ${tmuxID} session"
+else
+   tmux select-pane -t ${tmuxID}:1.2
+   tmux attach-session -t ${tmuxID}
+fi
+
+exit 0
