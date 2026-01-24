5df51962a0cb10544e4b2dd915f67eb9c2fb36e1
Richard Pogge <rwpogge@gmail.com>
2026-01-24 12:44:42 -0500

    Update mods.h
    advanced version to 3.2.2

--- mods/GUIs/modsUI/mods.h
+++ mods/GUIs/modsUI/mods.h
@@ -6,8 +6,8 @@
 //
 
 #define MODS_SESSION_NAME "MODS Control Panel" //!< Window Name for banner
-#define MODS_REV_NUMBER   "v3.2.2-archon"      //!< MODS UI Revision number
-#define MODS_REV_DATE     "2026 Jan 22"        //!< Revision date
+#define MODS_REV_NUMBER   "v3.2.3-archon"      //!< MODS UI Revision number
+#define MODS_REV_DATE     "2026 Jan 24"        //!< Revision date
 
 // Runtime files and paths
 
