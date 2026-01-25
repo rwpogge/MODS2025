5690f97b33d1b5a5bc7837416cb7558488d36100
Richard Pogge <rwpogge@gmail.com>
2026-01-25 13:54:55 -0500

    modsStatus v2 release
    released modsStatus v2, cleaned up

--- Scripts/modsStatus.pl
+++ Scripts/modsStatus.pl
@@ -30,6 +30,8 @@
 #   2025 Oct 13 - check if on a TMUX session [rwp/osu]
 #   2025 Dec 31 - added dataMan systemd service [rwp/osu]
 #
+#   2026 Jan 25 - adding remote ccdMon and azcam monitoring [rwp/osu]
+#
 #---------------------------------------------------------------------------
 
 use strict;
@@ -37,12 +39,32 @@ use warnings;
 use Curses;
 use POSIX qw(strftime);
 
+# Version number and date - dates in ISO8601 format
+
+my $verNum  = "v2.0.0";
+my $verDate = "2026-01-25";
+
 # Who are we and where are we running?
 
 my $hostID = `hostname`;
 chomp($hostID);
 my $modsID = uc($hostID);
 my $userID = (getpwuid($<))[0];
+
+# Remote server keys and hosts
+
+my $keyPath = "/lbt/lbto/mods/keys/id_ecdsa_mods";
+my $remPath = "/home/dts/bin";
+
+# Archon server hosts
+
+my $redHost = "192.168.139.131";
+my $blueHost = "192.168.139.132";
+
+if ($hostID eq "mods2") {
+    $redHost = "192.168.139.231";
+    $blueHost = "192.168.139.232";
+}
 
 # Initialize curses
 
@@ -95,16 +117,18 @@ my $offCol = COLOR_PAIR(6); # blue on black is Off (barely visible - no reliable
 
 my $userProcs = ["mmcServer","agwServer","redIMCS","blueIMCS","modsUI"]; # unix names
 my $userHead = ["Process","Status","UserID"];
+my $ccdHead = ["","Blue","Red"];
 
 my $sysdProcs = ["isis","lbttcs","modsenv","modsDD","dataMan"];
 my $sysdHead = ["Service","Status","Restart"];
 
-my @subSystems = qw(UTIL LLB GCAM WFS IEB HEB Archon IonGauge IEB HEB Archon IonGauge);
-my $pwrHead = ["Inst","Blue","Red"];
+my @subSystems = qw(UTIL LLB GCAM WFS IEB_B HEB_B Archon_B IGPower_B IEB_R HEB_R Archon_R IGPower_R);
+#my $pwrHead = ["Inst","Blue","Red"];
+my $pwrHead = ["","Blue ","Red "];
 
 # Update cadence
 
-my $cadence = 2; # seconds
+my $cadence = 1; # seconds
 
 # table column padding
 
@@ -122,14 +146,15 @@ attron(A_BOLD);
 
 while ($keepGoing) {
 
-    # what time is it?
+    #------------------------------------------------
+    #
+    # Instrument server status
+    #
 
     my $svcRow = 1;
     
     attron($headCol);
-    addstr($svcRow,1, "$modsID server status:"); 
-
-    # user services header
+    addstr($svcRow,1, "$modsID instrument server:");
 
     my $colNum = 1;
     $svcRow += 1;
@@ -138,9 +163,9 @@ while ($keepGoing) {
         $colNum++;
     }
     attroff($headCol);
-
-    # status of user procs
-
+ 
+    my $procUser = "";
+    
     $svcRow++;
     foreach my $procID (@$userProcs) {
         attron($dataCol);
@@ -155,7 +180,7 @@ while ($keepGoing) {
             attron($normCol);
             addstr($svcRow,$c0+1*$cpad,"running");
             attroff($normCol);
-            my $procUser = `ps eo user h -C $procID`;
+            $procUser = `ps eo user h -C $procID`;
             chomp($procUser);
             attron($dataCol);
             addstr($svcRow,$c0+2*$cpad,$procUser);
@@ -168,9 +193,12 @@ while ($keepGoing) {
         $svcRow++;
     }
 
-    # systemd services header
-
-    my $sysdRow = $svcRow + 1;
+    #------------------------------------------------
+    #
+    # Instrument server systemd service status
+    #
+    
+    my $sysdRow = 9;   # was $svcRow + 1;
 
     attron($headCol);
     addstr($sysdRow, 1, "$modsID systemd services:");
@@ -181,8 +209,6 @@ while ($keepGoing) {
         $colNum++;
     }
     attroff($headCol);
-
-    # status of systemd services
 
     $sysdRow++;
     my $colPair = $dataCol;
@@ -217,28 +243,29 @@ while ($keepGoing) {
         $sysdRow++
     }
 
+    #------------------------------------------------
+    #
     # MODS power status
     #
     # returned by vueinfo, in order
     #   iub llb gcam wfs ieb_b heb_b archon_b ig_b ieb_r heb_r archon_r ig_r
+    #
     
+    my $pwrRow = 17;  # was $sysdRow + 1;
+
     my $pwrStates = `/usr/local/bin/vueinfo pstatus`;
     chomp($pwrStates);
     my @sysPower = split(' ',$pwrStates);
     
-    # MODS subsystem power status header
-
-    my $pwrRow = $sysdRow + 1;
-
     attron($headCol);
     addstr($pwrRow, 1, "$modsID subsystem power:");
-    $pwrRow += 1;
-    $colNum = 1;
-    foreach my $hdrStr (@$pwrHead) {
-        addstr($pwrRow, $c0+($colNum-1)*$cpad, $hdrStr);
-        $colNum++;
-    }
-    attroff($headCol);
+#    $pwrRow += 1;
+#    $colNum = 1;
+#    foreach my $hdrStr (@$pwrHead) {
+#        addstr($pwrRow, $c0+($colNum-1)*$cpad, $hdrStr);
+#        $colNum++;
+#    }
+#    attroff($headCol);
 
     # power status of subsystems
 
@@ -249,6 +276,7 @@ while ($keepGoing) {
         clrtoeol();
     }
     $colPair = $dataCol;
+    my $subSys = "";
     foreach my $pCol ((0,1,2)) {
         foreach my $pRow ((0,1,2,3)) {
             my $iPwr = $pRow + 4*$pCol;
@@ -262,69 +290,161 @@ while ($keepGoing) {
                 $colPair = $headCol;
             }
             attron($colPair);
-            addstr($pwrRow+$pRow,$c0+$pCol*$cpad,$subSystems[$iPwr]);
+	    addstr($pwrRow+$pRow,$c0+$pCol*$cpad,$subSystems[$iPwr]);
             attroff($colPair);
         }
     }
 
-    # Dewar status: temperature and pressures
+    #------------------------------------------------
     #
-    # Retrieve using `vueinfo dewars` --> blueTemp bluePres redTemp redPres
+    # MODS CCD system status
     #
-    # later we'll introduce alarm states to the color coding...
+    
+    my $ccdRow = 23; # was $sysdRow + 1;
 
-    my $dewRow = $pwrRow + 5;
+    # Dewar temperatures and pressures read using vueinfo
+    
     my $dewTempPres = `/usr/local/bin/vueinfo dewars`;
     chomp($dewTempPres);
     my @dewTP = split(' ',$dewTempPres);
 
+    # MODS channel azcam and modsCCD status read remotely
+    
+    my $blueCCD = `ssh -i $keyPath mods\@$blueHost $remPath/ccdStatus`;
+    chomp($blueCCD);
+    my @blueStat = split(' ',$blueCCD);
+    
+    my $redCCD = `ssh -i $keyPath mods\@$redHost $remPath/ccdStatus`;
+    chomp($redCCD);
+    my @redStat = split(' ',$redCCD);
+
     # heading and parameters:
     
     attron($headCol);
-    addstr($dewRow,1,"$modsID dewars:");
+    addstr($ccdRow,1,"$modsID CCD status:");
+    $colNum = 1;
+    foreach my $hdrStr (@$ccdHead) {
+        addstr($ccdRow, $c0+($colNum-1)*$cpad, $hdrStr);
+        $colNum++;
+    }
     attroff($headCol);
+
     attron($dataCol);
-    addstr($dewRow+1,$c0,"Temperature:");
-    addstr($dewRow+2,$c0+3,"Pressure:");
+    addstr($ccdRow+1,$c0,"azcam-mods");
+    addstr($ccdRow+2,$c0,"modsCCD");
+    addstr($ccdRow+3,$c0,"Dewar Temp");
+    addstr($ccdRow+4,$c0,"Dewar Pres");
+    attroff($dataCol);
+
+    # azcam server status
+
+    $colPair = $normCol;
+
+    if ($blueStat[2] eq "running") {
+	$colPair = $normCol;
+	$procUser = $blueStat[3];
+    } else {
+	$colPair = $alertCol;
+	$procUser = "";
+    }
+    move($ccdRow+1,$c0+1*$cpad);
+    clrtoeol();
+    attron($colPair);
+    addstr($ccdRow+1,$c0+1*$cpad,$blueStat[2]);
+    attroff($colPair);
+    attron($dataCol);
+    addstr($ccdRow+1,$c0+1*$cpad+9,$procUser);
+    attroff($dataCol);
+
+    if ($redStat[2] eq "running") {
+	$colPair = $normCol;
+	$procUser = $redStat[3];
+    } else {
+	$colPair = $alertCol;
+	$procUser = "";
+    }
+    attron($colPair);
+    addstr($ccdRow+1,$c0+2*$cpad,$redStat[2]);
+    attroff($colPair);
+    attron($dataCol);
+    addstr($ccdRow+1,$c0+2*$cpad+9,$procUser);
+    attroff($dataCol);
+
+    # modsCCD agent status
+    
+    if ($blueStat[5] eq "running") {
+	$colPair = $normCol;
+	$procUser = $blueStat[6];
+    } else {
+	$colPair = $alertCol;
+	$procUser = "";
+    }
+    move($ccdRow+2,$c0+1*$cpad);
+    clrtoeol();
+    attron($colPair);
+    addstr($ccdRow+2,$c0+1*$cpad,$blueStat[5]);
+    attroff($colPair);
+    attron($dataCol);
+    addstr($ccdRow+2,$c0+1*$cpad+9,$procUser);
+    attroff($dataCol);
+
+    if ($redStat[5] eq "running") {
+	$colPair = $normCol;
+	$procUser = $redStat[6];
+    } else {
+	$colPair = $alertCol;
+	$procUser = "";
+    }
+    attron($colPair);
+    addstr($ccdRow+2,$c0+2*$cpad,$redStat[5]);
+    attroff($colPair);
+    attron($dataCol);
+    addstr($ccdRow+2,$c0+2*$cpad+9,$procUser);
     attroff($dataCol);
 
     # Temperature in C
 
     attron($normCol);
-    move($dewRow+1,$c0+1*$cpad);
+    move($ccdRow+3,$c0+1*$cpad);
     clrtoeol();
-    addstr($dewRow+1,$c0+1*$cpad,"$dewTP[0] C");
-    addstr($dewRow+1,$c0+2*$cpad,"$dewTP[2] C");
+    addstr($ccdRow+3,$c0+1*$cpad+2,"$dewTP[0] C");
+    addstr($ccdRow+3,$c0+2*$cpad+2,"$dewTP[2] C");
     attroff($normCol);
 
     # Pressure in torr
 
     attron($normCol);
-    move($dewRow+2,$c0+1*$cpad);
+    move($ccdRow+4,$c0+1*$cpad);
     clrtoeol();
-    addstr($dewRow+2,$c0+1*$cpad,"$dewTP[1] torr");
-    addstr($dewRow+2,$c0+2*$cpad,"$dewTP[3] torr");
+    addstr($ccdRow+4,$c0+1*$cpad,"$dewTP[1] torr");
+    addstr($ccdRow+4,$c0+2*$cpad,"$dewTP[3] torr");
     attroff($normCol);
-           
+
+    #------------------------------------------------
+    #
     # Bottom row: info update date/time
+    #
     
+    my $timeRow = 29; # was $ccdRow + 6;
     my @now = localtime;
     my $dateNow = strftime "%Y-%m-%d %H:%M:%S", @now;
     attron($headCol);
     if (defined $ENV{'TMUX'}) {
-	addstr($dewRow+4, 1, "Updated: $dateNow");
+	addstr($timeRow, 1, "Updated: $dateNow");
     }
     else {
-	addstr($dewRow+4, 1, "Updated: $dateNow (Ctrl+C to exit status)");
+	addstr($timeRow, 1, "Updated: $dateNow (Ctrl+C to exit status)");
     }
     attroff($headCol);
-    addstr($dewRow+5,0,"");
+    addstr($timeRow,0,"");
         
     # update the screen and sleep for $cadence
     
     refresh();
 
-    sleep($cadence);
+    if ($cadence > 0) {
+	sleep($cadence);
+    }
 } 
 
 attroff(A_BOLD);
